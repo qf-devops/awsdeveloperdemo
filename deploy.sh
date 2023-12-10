1c5a82a99e38a720989d7c157d214c4df23fefeb
ravi <ravi@vytec.com>
2023-12-10 21:33:38 +0530

    updated

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+
+# aws cloudformation create-stack \
+#   --stack-name myteststack \
+#   --template-body file:///Users/ravi/awsdeveloperdemo/ecs.yml \
+#   --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation delete-stack --stack-name myteststack
